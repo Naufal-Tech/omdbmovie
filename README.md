a4d2feffbad9a6b2cff926faa7fdceb5b5c78b2a
Naufal-Tech <naufal.geo11@gmail.com>
2022-11-11 22:44:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# DOKUMENTASI API:
+
+```
+https://documenter.getpostman.com/view/20393150/2s8YeptDAw
+```
+
+# OMDb Search
+
+Ini adalah OMDB - Movie search sederhana yang masih akan dikembangkan, dibuat menggunakan Express.Js - Node.Js dan database menggunakan MongoDB URI.
+
+## Setup
+
+Pastikan kamu telah install `node` dan `npm`.
+
+```terminal
+node -v
+npm -v
+```
+
+#### Setup .env file
+
+Buat .env file pada root dan tambahkan parameter:
+
+```
+REACT_APP_OMDB_API_KEY:
+DATABASE:
+JWT_SECRET:
+```
---
+++ server/routes/omdb.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+// GET request for OMDb data by search query
+router.get('/search', async (req, res) => {
+    try {
+        const searchQuery = req.query.s;
+        if (searchQuery != null) {
+            const api = `http://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`;
+            const response = await fetch(api, {headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }});
+            const responseJSON = await response.json();
+            res.json(responseJSON);
+        } else {
+            res.end();
+        } 
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// GET request for OMDb data by ID query
+router.get('/detail', async (req, res) => {
+    try {
+        const id = req.query.i;
+        const api = `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`;
+        const response = await fetch(api);
+        const responseJSON = await response.json();
+        res.json(responseJSON);
+    } catch(error) {
+        console.error(error);
+    }
+});
+
+module.exports = router;
