a824f2ce64e70ce4a1674c46396ea1bc7f3a06be
Naufal-Tech <naufal.geo11@gmail.com>
2022-11-14 03:04:36 +0700

    refactor

--- SOAL No.3 - Refactoring/refactor.js
+++ SOAL No.3 - Refactoring/refactor.js
@@ -1,6 +1,43 @@
-myString = "wo(k)e"
+function stringLength(str) {
+  return (str.length > 0) ? str : ""
+}
 
-var result = myString.substring( myString.indexOf( '(' ) + 1, myString.indexOf( ')' ) );
+function findFirstBracket(str) {
+  let indexFirstBracket = str.indexOf('(')
+  return indexFirstBracket
+}
 
+function findLastBracket(str) {
+  let indexLastBracket = str.indexOf(')')
+  return indexLastBracket
+}
 
-console.log(result);
+function findStringInBracket(str, indexFirst, indexLast) {
+  indexFirst += 1
+  return (indexLast >= 0) ? str.substring(indexFirst, indexLast) : ""
+}
+
+function getAnswer(str) {
+  let word = stringLength(str)
+  let indexFirstBracket = findFirstBracket(str)
+  let indexLastBracket = findLastBracket(str)
+  return (indexFirstBracket >= 0) ? findStringInBracket(word, indexFirstBracket, indexLastBracket) : ""
+}
+
+// Test cases
+let example = [
+    'naufal',
+    'nau(fal)abc',
+    '123(456)789',
+    '123(fal)456',
+    '123()123',
+    '()',
+    '(nau',
+    'fal('
+  ]
+
+let i = 0
+example.forEach(example => {
+  console.log(i, getAnswer(example));
+  i++
+});
